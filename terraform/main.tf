e0a96aee55c9ae5d245cfea38c65b1dc9783d8f1
Louis Fiori 🥖🧑‍💻 <louis.fioripro+github@gmail.com>
2024-06-18 15:42:52 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,9 +4,13 @@
 # - Create an OAI
 
 # Module 2:
-# - Create the Lambda function
+# - Create the Lambda function (to launch an instance)
 # - API Gateway (REST API)
 # - AGW Endpoint and Resource for the Lambda
 
 # Module 3:
 # - EC2 Launch Template
+
+# Module 4:
+# - Create the other Lambda function (to terminate an instance-
+# - Create a Cloudwatch alarms + a rule to trigger the Lambda
