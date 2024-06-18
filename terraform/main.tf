3bbe0eb4273a64de58273bf50121f8fa3e57bbc1
Louis Fiori 🥖🧑‍💻 <louis.fioripro+github@gmail.com>
2024-06-18 15:39:50 +0200

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,12 @@
+# Module 1:
+# - Create a S3 bucket + upload the frontend
+# - Create a Cloudfront distribution
+# - Create an OAI
+
+# Module 2:
+# - Create the Lambda function
+# - API Gateway (REST API)
+# - AGW Endpoint and Resource for the Lambda
+
+# Module 3:
+# - EC2 Launch Template
